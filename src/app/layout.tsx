6f628d090f204382ae6e710160184459bcb48948
Pencarisalmon <hsalmanazrian@gmail.com>
2025-05-26 18:19:41 +0700

    change client snap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from 'next/script'; 
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,7 +25,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-      <Script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="Mid-client-Vxcxv-rrp6GWWan3"/>
+        <Script
+          src="https://app.midtrans.com/snap/snap.js"
+          data-client-key="Mid-client-Vxcxv-rrp6GWWan3"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
